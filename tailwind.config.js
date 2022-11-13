9528b9794d9ac69fd4aca14aab5e6cbd2af65042
Chetra Seng <sengchetra.love@icloud.com>
2022-11-13 20:24:06 +0700

    Added color and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary-100": "#7A28CB",
-        "primary-80": "#9955DD",
-        "primary-60": "#B27FE6",
-        "primary-40": "#CCAAEE",
-        "primary-20": "#E5D4F7",
-        "primary-dark": "#19082B",
-        "secondary-100": "#4361EE",
-        "secondary-80": "#4463EE",
-        "secondary-60": "#738AF2",
-        "secondary-40": "#A1B1F7",
-        "secondary-20": "#D0D8FB",
-        "tertiary-100": "#4CC9F0",
-        "tertiary-80": "#43C7EF",
-        "tertiary-60": "#72D5F3",
-        "tertiary-40": "#A1E3F7",
-        "tertiary-20": "#D0F1FB",
+        "primary": {
+          100: "#7A28CB",
+          80: "#9955DD",
+          60: "#B27FE6",
+          40: "#CCAAEE",
+          20: "#E5D4F7",
+          "dark": "#19082B"
+        },
+        "secondary": {
+          100: "#4361EE",
+          80: "#4463EE",
+          60: "#738AF2",
+          40: "#A1B1F7",
+          20: "#D0D8FB"
+        },
+        "tertiary": {
+          100: "#4CC9F0",
+          80: "#43C7EF",
+          60: "#72D5F3",
+          40: "#A1E3F7",
+          20: "#D0F1FB"
+        },
         "success": "#76FF6A",
         "error": "#FB3640",
-        "input": "#E3E3E3"
+        "input": "#E3E3E3",
+        "white": "#FFFFFF",
+        "text1": "#140722",
+        "text2": "#393939"
       },
       keyframes: {
         "slide-in": {
