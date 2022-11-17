4b60c7eee2d5da855ad7108b69ee605513d95bca
Chetra Seng <sengchetra.love@icloud.com>
2022-11-17 18:04:54 +0700

    Added navigation

--- src/component/Voter/EndVoteSession/index.jsx
+++ src/component/Voter/EndVoteSession/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Button from "../../Button"
+import Button from "../../Button";
+import { useNavigate } from "react-router-dom";
+
 const EndVoteSession = () => {
-    return (
-        <div className=" w-[450px] flex flex-col gap-5 justify-center items-start p-9 rounded-md shadow-md">
-            <h1 className="font-medium text-[1.75rem] text-slate-900">End Vote Session</h1>
-            <p className="font-medium text-sm text-gray-700">
-            The vote session has end now 
-            click on button below to view the results.
-            </p>
-            <Button onClick = {() => {}} name="View Result"/>
-        </div>
-    );
+	const navgivate = useNavigate();
+
+  return (
+    <div className=" w-[450px] flex flex-col gap-5 justify-center items-start p-9 rounded-md shadow-md">
+      <h1 className="font-medium text-[1.75rem] text-slate-900">
+        End Vote Session
+      </h1>
+      <p className="font-medium text-sm text-gray-700">
+        The vote session has end now click on button below to view the results.
+      </p>
+      <Button onClick={() => navgivate("/votes")} name="View Result" />
+    </div>
+  );
 };
 
 export default EndVoteSession;
